1fad968add67279127f1306a92e12fbd4ba3a231
Bruno Campos <bruno.b.campos@capgemini.com>
2023-10-16 23:08:59 -0300

    correcao no codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,63 +3,67 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'assignment2';
 
   listToBuy = [
     {
-      nome: "Blusa",
-      quantidade: "10"
+      nome: 'Blusa',
+      quantidade: '10',
     },
     {
-      nome: "Calca",
-      quantidade: "2"
+      nome: 'Calca',
+      quantidade: '2',
     },
     {
-      nome: "Meias",
-      quantidade: "5"
+      nome: 'Meias',
+      quantidade: '5',
     },
     {
-      nome: "Moletom",
-      quantidade: "1"
+      nome: 'Moletom',
+      quantidade: '1',
     },
     {
-      nome: "Bolsa",
-      quantidade: "2"
-    }
+      nome: 'Bolsa',
+      quantidade: '2',
+    },
   ];
 
   allBuy = false;
   nothingBuy = true;
-  
-  trocaColuna = (itemm: object) => {
+
+  trocaColuna = (itemIndex: number) => {
     //logica pra troca o "li" pro outro lado - itemIndex: number
-    this.nothingBuy = false; 
+    this.nothingBuy = false;
     console.log(this.nothingBuy);
-    //this.addItem(itemIndex)
-    //Recebe-se o objeto no
-    console.log(itemm)
-    
-    //this.removeItem(itemIndex)
-  }
-  items: object[] = []
-  /*
+    console.log(itemIndex);
+
+    this.addItem(itemIndex);
+    this.removeItem(itemIndex);
+    if (this.listToBuy.length === 0) 
+      this.allBuy = true;
+  };
+
+  items: {
+    nome: string;
+    quantidade: string;
+  }[] = [];
 
   addItem = (itemIndex: number) => {
-    let item = {
+    let items = {
       nome: this.listToBuy[itemIndex].nome,
-      quantidade: this.listToBuy[itemIndex].quantidade
-    }
-    this.items.push(item);
-  }
+      quantidade: this.listToBuy[itemIndex].quantidade,
+    };
+    this.items.push(items);
+  };
 
   getItems = () => {
     return this.items;
-  }
-  */
+  };
+
   removeItem = (itemIndex: number) => {
-    this.items.splice(itemIndex, 1)
-  }
+    this.listToBuy.splice(itemIndex, 1);
+  };
 }
